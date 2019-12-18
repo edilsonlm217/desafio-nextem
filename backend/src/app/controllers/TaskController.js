42f6e0b3cafb94dc0a30e0c528eb346d7c797601
edilsonlm217 <edilsonlm217@hotmail.com>
2019-12-18 17:14:29 -0400

    cria middleware de autenticação e métodos index, store, update do task controller

--- backend/src/app/controllers/TaskController.js
+++ backend/src/app/controllers/TaskController.js
@@ -1,8 +1,38 @@
-import Taks from '../models/Task';
+import Task from '../models/Task';
 
 class TaskController {
     async store(req, res) {
-        return res.json({ok: true});    
+        const { description, status, deadline, user_id } = req.body;
+
+        const response = await Task.create({
+            description,
+            status,
+            deadline,
+            user_id
+        });
+
+        return res.json(response);    
+    }
+
+    async update(req, res) {
+        const { description, status, deadline, user_id } = req.body;
+
+        const task = await Task.findByPk(req.params.taskId);
+
+        const response = await task.update({
+            description,
+            status,
+            deadline,
+            user_id,
+        });
+
+        return res.json(response);    
+    }
+
+    async index(req, res) {
+        const response = await Task.findAll();
+
+        return res.json(response);
     }
 }
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
+import TaskController from './app/controllers/TaskController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/task', TaskController.store);
+routes.post('/task/:taskId', TaskController.update);
+routes.get('/task', TaskController.index);
 
 export default routes;
