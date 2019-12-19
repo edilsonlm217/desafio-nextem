56ee8af5b3ea3eaca890a16cc6fa319df45e285d
edilsonlm217 <edilsonlm217@hotmail.com>
2019-12-19 08:57:50 -0400

    cria layouts para tela de login e demais telas

---
+++ frontend/src/pages/_layouts/auth/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    height: 100%;
+    background: rgb(83,61,109);
+    background: linear-gradient(142deg, rgba(83,61,109,1) 0%, rgba(57,93,168,1) 51%, rgba(83,61,109,1) 100%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
